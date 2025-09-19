f1db04e39ddd68a8449c1aafb3ccf9dab7f52251
overajhe92e <ardrey2200@gmail.com>
2025-09-19 19:50:28 -0400

    changed recluse seal

--- seals/recluseseye.lua
+++ seals/recluseseye.lua
@@ -7,25 +7,13 @@ SMODS.Seal {
         }
     },
     badge_colour = HEX('000000'),
-   loc_txt = {
-        name = 'Recluses Eye',
-        label = 'Recluses Eye',
-        text = {
-        [1] = 'Retriggers scored card 3 times.'
-    }
-    },
     atlas = 'CustomSeals',
     unlocked = true,
     discovered = true,
     no_collection = false,
     calculate = function(self, card, context)
-        if context.repetition and card.should_retrigger then
+        if context.repetition then
             return { repetitions = card.ability.seal.extra.retrigger_times }
-        end
-        if context.main_scoring and context.cardarea == G.play then
-            card.should_retrigger = false
-            card.should_retrigger = true
-            card.ability.seal.extra.retrigger_times = 3
         end
     end
 }
