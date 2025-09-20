b53bdd101a4e5bd5af06c163d3ad79c73df05706
overajhe92e <ardrey2200@gmail.com>
2025-09-19 21:34:22 -0400

    no collection because i don't have an astro sprite yet

--- jokers/otherocs.lua
+++ jokers/otherocs.lua
@@ -64,6 +64,7 @@ SMODS.Joker {
             suit = 'Spades'
         }
     },
+    no_collection = true,
 
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.eechips } }
