e999e57068b38ad30626b36e9bfa36181b7aae7a
overajhe92e <ardrey2200@gmail.com>
2025-09-19 23:43:43 -0400

    used for jokers inpool

---
+++ data/otherchecks.lua
@@ -0,0 +1,15 @@
+function unbalancedstuff()
+    if ocstobal.configbs.unbalanced_mode == true then
+        return true
+    else
+        return false
+    end
+end
+
+function almanacstuff()
+    if ocstobal.configbs.very_unbalanced_mode == true and ocstobal.configbs.very_unbalanced_mode == true then
+        return true
+    else
+        return false
+    end
+end
