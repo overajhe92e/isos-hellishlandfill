495963d46df645a3da055c48f34ec4cb7ed5f53b
overajhe92e <ardrey2200@gmail.com>
2025-10-14 07:25:03 -0400

    fake jokers blehh

--- jokers/fake ass mfs.lua
+++ jokers/fake ass mfs.lua
@@ -2,9 +2,8 @@ SMODS.Joker {
     key = 'fakeseraph',
     cost = 11,
     rarity = 3,
-    add_to_deck = function(self)
+    add_to_deck = function(self,card)
         SMODS.add_card{key='j_ocstobal_somethingevil'}
-        Card:shatter(self)
-        attention_text = ({text = 'Faked!'})
+        SMODS.destroy_cards(card,nil,nil,true)
     end
 }
