17fbf5a8b65b6c5ea64d09376032a3e04cd08445
overajhe92e <ardrey2200@gmail.com>
2026-04-23 13:31:50 -0400

    get rid of the useless scoring parameter

--- jokers/RYOSHU STOP IT.lua
+++ jokers/RYOSHU STOP IT.lua
@@ -1,44 +1,3 @@
-SMODS.Scoring_Calculation({
-    key = "muga",
-    func = function(self, chips, mult, flames)
-        return chips * mult
-    end,
-    parameters = { 'chips', 'mult' },
-    replace_ui = function(self)
-        local scale = 0.3
-        return
-        {
-            {
-                n = G.UIT.C,
-                config = { align = 'cm', id = 'hand_chips' },
-                nodes = {
-                    SMODS.GUI.score_container({
-                        type = 'chips',
-                        text = '?',
-                        align = 'cm',
-                        w = 1.2,
-                        scale = scale
-                    })
-                }
-            },
-            SMODS.GUI.operator(scale * 0.75),
-            {
-                n = G.UIT.C,
-                config = { align = 'cm', id = 'hand_mult' },
-                nodes = {
-                    SMODS.GUI.score_container({
-                        type = 'mult',
-                        text = '?',
-                        align = 'cm',
-                        w = 1.2,
-                        scale = scale
-                    })
-                }
-            }
-        }
-    end
-})
-
 SMODS.Joker {
     key = "muga",
     cost = 12,
@@ -47,6 +6,7 @@ SMODS.Joker {
         G.GAME.iso_hide_score = true
     end,
     remove_from_deck = function(self, card, from_debuff)
+        G.GAME.iso_hide_score = false
     end,
     calculate = function(self, card, context)
         if context.joker_main then
